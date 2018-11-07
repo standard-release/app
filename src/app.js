9274f3e545ca0f5c89edaea962612bb6164db354
Charlike Mike Reagent <mameto2011@gmail.com>
2018-11-08 01:35:40 +0200

    fix: debugginggggggggg
    Signed-off-by: Charlike Mike Reagent <mameto2011@gmail.com>

--- src/app.js
+++ src/app.js
@@ -72,8 +72,8 @@ async function getPkgMeta(context, robot) {
 
   // Do we need such thing as "commits since last tag"?
   const allCommitsSinceLastTag = commits.map((commit) => {
-    robot.log(commit);
-    robot.log('========================');
+    console.log(commit);
+    console.log(commit.message);
     const cmt = parse(commit.message);
     cmt.sha = commit.sha;
     cmt.author = context.payload.author;
